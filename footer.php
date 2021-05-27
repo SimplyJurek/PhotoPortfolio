a904bb2c45f40f0c39ff7a7803de8b16257cb617
Jerzy Baldy <simply.jurek@icloud.com>
2021-05-27 19:46:43 +0200

    Code cleanup

--- footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer-container">
-            <address class="footer-text">
-                Made by <a href="#" class="footer-link">Jerzy Baldy</a>, 2021
-            </address>
-        </footer>
