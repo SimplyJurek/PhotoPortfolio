34177cda53cd2623e0c64c853cf04366fc5af324
Jerzy Baldy <simply.jurek@icloud.com>
2021-05-28 01:26:04 +0200

    Photos Added

--- gallery.js
+++ gallery.js
@@ -2,6 +2,12 @@ const thumbGrid = document.querySelector(".thumbnails-gallery");
 const template = document.querySelector(".thumbnail-template");
 const slidePlayer = document.querySelector(".slide-player");
 
+//NUMBER OF IMAGES IN THE GALLLERY
+
+const numOfImg = 20;
+
+//-------------------------------
+
 let thumbs = [];
 
 class Thumbnail {
@@ -14,18 +20,18 @@ class Thumbnail {
 }
 imgArr = new Array();
 
-for( var i = 1; i <= 7; i++) {
+for( var i = 1; i <= numOfImg; i++) {
     imgArr[i] = "Gallery/" + i + ".jpeg";
 }
 
-for( var i = 1; i <= 7; i++) {
+for( var i = 1; i <= numOfImg; i++) {
     thumbs[i] = new Thumbnail(template,i);
     thumbGrid.appendChild(thumbs[i].getClone());
 }
 
 const imgSelector = document.querySelectorAll(".thumbnail-img");
 
-for(var i = 0; i < 7; i++) {
+for(var i = 0; i < numOfImg; i++) {
     imgSelector[i].src = imgArr[i+1];
 }
     
